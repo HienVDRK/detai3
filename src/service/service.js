931ed9150180baa36ea5b7869702d9c9804667eb
HienVDRK <hienvd@rikkeisoft.com>
2020-03-04 17:28:19 +0700

    chuyen sang serverRuntimeConfig

--- src/service/service.js
+++ src/service/service.js
@@ -2,22 +2,22 @@
 import getConfig from 'next/config'
 import axiosConfig from '../../axiosConfig'
 const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
-
+console.log(serverRuntimeConfig.APIKEY)
 export const getFilmsByTitle = async (keyWord) => {
-  const response = await axiosConfig.get(`/?s=${keyWord}&apikey=${publicRuntimeConfig.APIKEY}`)
+  const response = await axiosConfig.get(`/?s=${keyWord}&apikey=${serverRuntimeConfig.APIKEY}`)
   if (response.data) {
     return response.data
   }
 }
 
 export const getDetailFilmsById = async (keyWord) => {
-  const response = await axiosConfig.get(`/?i=${keyWord}&apikey=${publicRuntimeConfig.APIKEY}`)
+  const response = await axiosConfig.get(`/?i=${keyWord}&apikey=${serverRuntimeConfig.APIKEY}`)
   if (response.data) {
     return response.data
   }
 }
 export const getFilmsByTitleandYear = async (title, year) => {
-  const response = await axiosConfig.get(`/?s=${title}&y=${year}&apikey=${publicRuntimeConfig.APIKEY}`)
+  const response = await axiosConfig.get(`/?s=${title}&y=${year}&apikey=${serverRuntimeConfig.APIKEY}`)
   if (response.data) {
     return response.data
   }
