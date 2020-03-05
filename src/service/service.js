d5c5420c2a7f30fad14900eae11cda182d342daa
HienVDRK <hienvd@rikkeisoft.com>
2020-03-05 08:56:25 +0700

    check serverRuntimeconfig

--- src/service/service.js
+++ src/service/service.js
@@ -2,22 +2,23 @@
 import getConfig from 'next/config'
 import axiosConfig from '../../axiosConfig'
 const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
-console.log(serverRuntimeConfig.APIKEY)
+const apiKey = serverRuntimeConfig.APIKEY ? serverRuntimeConfig.APIKEY : publicRuntimeConfig.APIKEY
+console.log(apiKey)
 export const getFilmsByTitle = async (keyWord) => {
-  const response = await axiosConfig.get(`/?s=${keyWord}&apikey=${serverRuntimeConfig.APIKEY}`)
+  const response = await axiosConfig.get(`/?s=${keyWord}&apikey=${apiKey}`)
   if (response.data) {
     return response.data
   }
 }
 
 export const getDetailFilmsById = async (keyWord) => {
-  const response = await axiosConfig.get(`/?i=${keyWord}&apikey=${serverRuntimeConfig.APIKEY}`)
+  const response = await axiosConfig.get(`/?i=${keyWord}&apikey=${apiKey}`)
   if (response.data) {
     return response.data
   }
 }
 export const getFilmsByTitleandYear = async (title, year) => {
-  const response = await axiosConfig.get(`/?s=${title}&y=${year}&apikey=${serverRuntimeConfig.APIKEY}`)
+  const response = await axiosConfig.get(`/?s=${title}&y=${year}&apikey=${apiKey}`)
   if (response.data) {
     return response.data
   }
